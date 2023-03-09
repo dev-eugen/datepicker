2ff3ff56c17f19560b421b17c9e70ce09461624f
OleksandrYastrebov <frontwork0405@gmail.com>
2023-03-09 19:51:42 +0200

    fix: adopting styles to buttons

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,25 +1,28 @@
 <template>
     <div class="wrapper">
-        <Datepicker v-model="selectedDate"  :markers=" [
-          {
-            date: addDays(new Date(), 1),
-            type: 'dot',
-            tooltip: [{ text: 'Dot with tooltip', color: 'green' }],
-          },
-          {
-            date: addDays(new Date(), 2),
-            type: 'dot',
-            tooltip: [
-              { text: 'First tooltip', color: 'blue' },
-              { text: 'Second tooltip', color: 'yellow' },
-            ],
-          },
-          {
-            date: addDays(new Date(), 3),
-            type: 'dot',
-            color: 'yellow',
-          },
-        ]"  placeholder="Select Date" />
+        <Datepicker
+v-model="selectedDate"  :markers=" [
+                {
+                    date: addDays(new Date(), 1),
+                    type: 'dot',
+                    tooltip: [{ text: 'Dot with tooltip', color: 'green' }],
+                },
+                {
+                    date: addDays(new Date(), 2),
+                    type: 'dot',
+                    tooltip: [
+                        { text: 'First tooltip', color: 'blue' },
+                        { text: 'Second tooltip', color: 'yellow' },
+                    ],
+                },
+                {
+                    date: addDays(new Date(), 3),
+                    type: 'dot',
+                    color: 'yellow',
+                },
+            ]"
+            placeholder="Select Date"
+        />
     </div>
 </template>
 
@@ -34,6 +37,6 @@
 <style lang="scss">
     @import 'src/VueDatePicker/style/main';
     .wrapper {
-        padding: 200px;
+        padding: 100px;
     }
 </style>
